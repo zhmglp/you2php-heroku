c5d43aad471b2d6571560cda11fea087e896a558
zhmglp <stsxsy@outlook.com>
2018-06-06 23:18:42 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -4,6 +4,6 @@ define('APIKEY', 'AIzaSyBQGs_2WFu2nWowyu32JkTrj3UzGpPp57M');//这里改成您的
 define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435eE67CtzDSh4');//一个随机字符串，加解密KEY
+define('EN2DEKEY', 'AnQUqP1XDeEF435eE67CtzDSh4');//一个随机字符串，加解密KEY
 define('EMAIL', '123@123.com');//邮箱
 ?>
